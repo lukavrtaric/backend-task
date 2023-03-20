48f1f328dddeed41aa4fa5e879d982bdb5fc06c5
Luka <luka.vrtaric@biss.hr>
2023-03-20 07:57:00 +0100

    added mongodb cloud creds

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user.module';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
+    imports: [
+        MongooseModule.forRoot(
+            'mongodb+srv://vercel-admin-user:0103990Luka@cluster0.0xbmhrc.mongodb.net/?retryWrites=true&w=majority',
+        ),
+        UserModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
